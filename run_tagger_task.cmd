14c1faf5a39fdf751be47b909c2ef1908df16caa
sportdogfood <philip@sportdogfood.com>
2026-03-13 17:56:38 -0400

    zc

--- run_tagger_task.cmd
+++ run_tagger_task.cmd
@@ -8,6 +8,8 @@ cd /d "C:\Users\gombc\OneDrive - Sport Dog Food\github\repos\ringstatus" || exit
 echo [%DATE% %TIME%] TAGGER RUN >> "C:\actions-runner\ringstatus\epoch-tagger.log"
 "C:\Program Files\nodejs\node.exe" tagger.js >> "C:\actions-runner\ringstatus\epoch-tagger.log" 2>&1
 
+timeout /t 30 /nobreak >nul
+
 echo [%DATE% %TIME%] PUBLISH RUN >> "C:\actions-runner\ringstatus\publisher.log"
 "C:\Program Files\nodejs\node.exe" "C:\Users\gombc\OneDrive - Sport Dog Food\github\repos\ringstatus\publisher.js" >> "C:\actions-runner\ringstatus\publisher.log" 2>&1
 exit /b %ERRORLEVEL%
