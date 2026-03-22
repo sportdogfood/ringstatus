ddcb423425864d09a19761476112a901c12b15e7
sportdogfood <philip@sportdogfood.com>
2026-03-22 12:43:56 -0400

    Update run_tagger_task.cmd

--- run_tagger_task.cmd
+++ run_tagger_task.cmd
@@ -7,9 +7,15 @@ cd /d "C:\Users\gombc\OneDrive - Sport Dog Food\github\repos\ringstatus" || exit
 
 echo [%DATE% %TIME%] TAGGER RUN >> "C:\actions-runner\ringstatus\epoch-tagger.log"
 "C:\Program Files\nodejs\node.exe" tagger.js >> "C:\actions-runner\ringstatus\epoch-tagger.log" 2>&1
+if errorlevel 1 exit /b %ERRORLEVEL%
+
+echo [%DATE% %TIME%] HEARTBEAT_PATTERNS RUN >> "C:\actions-runner\ringstatus\epoch-tagger.log"
+"C:\Program Files\nodejs\node.exe" heartbeat_patterns.js >> "C:\actions-runner\ringstatus\epoch-tagger.log" 2>&1
+if errorlevel 1 exit /b %ERRORLEVEL%
 
 echo [%DATE% %TIME%] TRIPS_TAGGER RUN >> "C:\actions-runner\ringstatus\trips-tagger.log"
 "C:\Program Files\nodejs\node.exe" trips_tagger.js >> "C:\actions-runner\ringstatus\trips-tagger.log" 2>&1
+if errorlevel 1 exit /b %ERRORLEVEL%
 
 timeout /t 30 /nobreak >nul
 
