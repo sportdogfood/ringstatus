277584e14b5be289792f5227e0327ab3b396153d
sportdogfood <philip@sportdogfood.com>
2026-04-15 13:00:35 -0400

    Update # inspect.ps1

--- # inspect.ps1
+++ # inspect.ps1
@@ -19,3 +19,40 @@ Enable-ScheduledTask -TaskName 'epoch-tagger-local'
 Get-ScheduledTask -TaskName 'epoch-tagger-local' |
   ForEach-Object { $_.Settings } |
   Format-List MultipleInstances, Enabled
+
+
+  Start-Process powershell -Verb RunAs
+
+
+  $settings = New-ScheduledTaskSettingsSet `
+  -MultipleInstances Queue `
+  -ExecutionTimeLimit (New-TimeSpan -Hours 72) `
+  -Hidden `
+  -Priority 7
+
+Set-ScheduledTask -TaskName 'epoch-tagger-local' -Settings $settings
+Enable-ScheduledTask -TaskName 'epoch-tagger-local'
+
+
+$task = Get-ScheduledTask -TaskName 'epoch-tagger-local'
+
+$action    = $task.Actions
+$trigger   = $task.Triggers
+$principal = $task.Principal
+$settings  = New-ScheduledTaskSettingsSet `
+  -MultipleInstances Queue `
+  -ExecutionTimeLimit (New-TimeSpan -Hours 72) `
+  -Hidden `
+  -Priority 7
+
+Register-ScheduledTask `
+  -TaskName 'epoch-tagger-local' `
+  -Action $action `
+  -Trigger $trigger `
+  -Principal $principal `
+  -Settings $settings `
+  -Force
+
+  Get-ScheduledTask -TaskName 'epoch-tagger-local' |
+  ForEach-Object { $_.Settings } |
+  Format-List MultipleInstances, Enabled
